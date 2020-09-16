1dce19cd91f22243549f0fd7069f140e18a43489
Liselle Logan <liselle.logan@gmail.com>
2020-09-16 17:30:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   if array.size > 3
     array.join(", ")
   elsif array.size == 3
+    array[-1] = "and " + array[-1]
     array.join (", ")
-    array[-1].insert(0, "and ")
+    array.join(" and ")
   elsif array.size == 2
     array.join(" and ")
   else
