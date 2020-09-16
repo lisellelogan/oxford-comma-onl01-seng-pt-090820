e1ba121158d8b08bf178fa007e6774f462e290f9
Liselle Logan <liselle.logan@gmail.com>
2020-09-16 17:13:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size > 3
+    array.join(", ")
+  elsif array.size == 3
+    array.join (", ")
+    array[-1].insert(0, "and ")
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    array[0]
+  end
 end
