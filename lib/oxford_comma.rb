6d6fc4fd19838e91dfdbe0cc80466446b0f5a265
Liselle Logan <liselle.logan@gmail.com>
2020-09-16 17:32:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   if array.size > 3
     array.join(", ")
   elsif array.size == 3
+    array_modified = array
     array[-1] = "and " + array[-1]
-    array.join (", ")
+    array_modified.join (", ")
   elsif array.size == 2
     array.join(" and ")
   else
